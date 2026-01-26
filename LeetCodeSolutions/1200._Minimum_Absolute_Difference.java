99252b36c3e56508de6f24a66ac302628adb26a7
JAGDISH <js8724285@gmail.com>
2026-01-26 12:24:08 +0530

    Add POD of LeetCode date:26-01-2026

---
+++ LeetCodeSolutions/1200._Minimum_Absolute_Difference.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+        Arrays.sort(arr);
+        int min=Integer.MAX_VALUE;
+        for(int i=1;i<arr.length;i++){
+            min=Math.min(min,(arr[i]-arr[i-1]));
+        }
+        List<List<Integer>> l=new ArrayList<>();
+        for(int i=1;i<arr.length;i++){
+            if((arr[i]-arr[i-1])==min){
+                l.add(Arrays.asList(arr[i - 1], arr[i]));
+            }
+        }
+        return l;
+
+
+    }
+}
